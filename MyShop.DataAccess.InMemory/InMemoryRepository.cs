db3c00b9b24d654acaf9bc8d7407e6462603a278
level7up <abdoleemso200@gmail.com>
2021-11-19 16:46:01 +0200

    Updates

--- MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,16 +9,16 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository <T> where T : BaseEntity
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
-        ObjectCache cache =  MemoryCache.Default;
+        ObjectCache cache = MemoryCache.Default;
         List<T> items;
         string className;
         public InMemoryRepository()
         {
             className = typeof(T).Name;
             items = cache[className] as List<T>;
-            if(items == null)
+            if (items == null)
             {
                 items = new List<T>();
             }
@@ -27,12 +28,13 @@ namespace MyShop.DataAccess.InMemory
             cache[className] = items;
         }
 
-        public void Insert (T t)
+
+        public void Insert(T t)
         {
             items.Add(t);
 
         }
-        public void Update (T t)
+        public void Update(T t)
         {
             T tToUpdate = items.Find(i => i.Id == t.Id);
             if (tToUpdate != null)
@@ -63,7 +65,7 @@ namespace MyShop.DataAccess.InMemory
             return items.AsQueryable();
 
         }
-        public void Delete (string Id)
+        public void Delete(string Id)
         {
             T tToDelete = items.Find(i => i.Id == Id);
             if (tToDelete != null)
