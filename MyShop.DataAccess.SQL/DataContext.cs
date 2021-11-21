dfa78d9456ae0d1f1021ce54baa349682cbaf051
level7up <abdoleemso200@gmail.com>
2021-11-21 15:11:25 +0200

    BasketService

--- MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop.DataAccess.SQL/DataContext.cs
@@ -10,11 +10,14 @@ namespace MyShop.DataAccess.SQL
 {
     public class DataContext : DbContext
     {
-        public DataContext(): base("DefaultConnection"){
-           
+        public DataContext() : base("DefaultConnection") {
+
         }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> basketItems { get; set; }
     }
 }
